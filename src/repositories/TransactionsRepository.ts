64dbd3d977f777f1fe720e2f5f2cbaadaa08704a
Caubert Castro <caubert.castro@gmail.com>
2020-04-20 01:39:38 -0300

    Adicionado os métodos solicitados no exercio dois

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acc, transaction) => {
+      return transaction.type !== 'income' ? acc : acc + transaction.value;
+    }, 0);
+
+    const outcome = this.transactions.reduce((acc, transaction) => {
+      return transaction.type !== 'outcome' ? acc : acc + transaction.value;
+    }, 0);
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
